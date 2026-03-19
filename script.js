9b0618352b39cd981a2bc8616098032d0723c1bf
Md Nouman <noumantannu06@gmail.com>
2026-03-19 17:50:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function mincost(arr)
 { 
-//write your code here
+//write your code hereif (arr.length <= 1) return 0;
+    let totalCost = 0;
+    while (arr.length > 1) {
+        arr.sort((a, b) => a - b);
+        let first = arr.shift();
+        let second = arr.shift();
+        let cost = first + second;
+        totalCost += cost;
+        arr.push(cost);
+    }
+    
 // return the min cost
-  
+  return totalCost;
 }
 
 module.exports=mincost;
